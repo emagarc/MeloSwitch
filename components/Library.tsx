03251728001d3dd35b105a3af421947166167a81
Emanuel Garcia <wemagw@gmail.com>
2023-06-25 13:45:39 -0300

    Layout

---
+++ components/Library.tsx
@@ -0,0 +1,54 @@
+"use client";
+
+import { TbPlaylist } from "react-icons/tb";
+import { AiOutlinePlus } from "react-icons/ai";
+
+const Library = () => {
+    const onClick = () => {
+        // Handle upload latter
+    }
+  return (
+    <div className="flex flex-col">
+        <div
+            className="
+                flex items-center pt-4
+                justify-between px-5
+            "
+        >
+            <div
+                className="
+                    inline-flex items-center 
+                    gap-x-2
+                "
+            >
+                <TbPlaylist 
+                    size={26} 
+                    className="text-neutral-400"
+                />
+                <p className="text-neutral-400 font-medium text-md">
+                    Your Library
+                </p>
+            </div>
+            <AiOutlinePlus 
+                onClick={onClick}
+                size={20}
+                className="
+                    text-neutral-400
+                    cursor-pointer transition
+                    hover:text-white
+                "
+            />
+        </div>
+        <div 
+            className="
+                flex flex-col gap-y-2
+                mt-4 px-3    
+            "
+        >
+            List of Songs!
+        </div>
+    </div>
+  )
+}
+
+export default Library;
